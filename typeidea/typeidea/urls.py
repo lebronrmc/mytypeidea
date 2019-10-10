c41238b47c00da04d2db2e4f090874e6454aed02
lebronrmc <lebronrmc@gmail.com>
2019-10-10 22:42:09 +0800

    更新提交

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -15,7 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from blog.views import (
+    IndexView, CategoryView,TagView,
+    PostDetailView,
+)
+from config.views import links
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #url(r'^$',post_list),
+    #url(r'category/(?P<category_id>\d+)/$',post_list),
+    #url(r'tag/(?P<tag_id>\d+)/$', post_list),
+    #url(r'post/(?P<post_id>\d+).html$', post_detail) ,
+    #url(r'links/$', links),
+    #url(r'^super_admin/',admin.site.urls) ,
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    url(r'tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    url(r'post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
+    url(r'links/$', links, name='links'),
+    url(r'^super_admin/', admin.site.urls, name='super-admin'),
 ]
